c0b5e71475e6ec73acd111c4cdbbe6696a57649f
philgetz <felips.110@gmail.com>
2017-04-06 14:36:19 -0700

    further work on compnent main page

--- app/routes/blog.js
+++ app/routes/blog.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    model: function() {
+
+        let promise = this.store.findAll('post');
+
+        return promise;
+    }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,9 +2,24 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model: function() {
+        var introCardsContent = [{
+            title: 'Latest Work',
+            text: '',
+            follow: 'See Portfolio',
+            other: ''
+        }, {
+            title: 'Latest Blog Post',
+            text: '',
+            follow: 'Go to Blog',
+            other: ''
+        }, {
+            title: 'About Me',
+            text: '',
+            follow: 'Get to Know Me',
+            other: ''
+        }];
 
-        let promise = this.store.findAll('post');
-
-        return promise;
+        return introCardsContent
     }
+
 });
