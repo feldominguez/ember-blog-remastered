874789d43c4022dac9b67daf018c12196afdb30b
felipe <felips.110@gmail.com>
2017-04-09 21:35:25 -0700

    setting up for card section

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,17 +3,20 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model: function() {
         var introCardsContent = [{
-            title: 'Latest Work',
+            sectionTitle: 'Latest Work',
+            cardTitle: 'Simple Blog',
             text: '',
             follow: 'See Portfolio',
             other: ''
         }, {
-            title: 'Latest Blog Post',
+            sectionTitle: 'Latest Blog Post',
+            cardTitle: 'Learn to use helpers in Ember',
             text: '',
             follow: 'Go to Blog',
             other: ''
         }, {
-            title: 'About Me',
+            sectionTitle: 'About Me',
+            cardTitle: 'Front End Everything',
             text: '',
             follow: 'Get to Know Me',
             other: ''
