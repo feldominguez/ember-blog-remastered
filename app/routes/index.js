7180f032cae704c721837f62c8834dd9ba8042ce
felipe <felips.110@gmail.com>
2017-04-09 22:53:08 -0700

    adding styles to cards

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,21 +4,21 @@ export default Ember.Route.extend({
     model: function() {
         var introCardsContent = [{
             sectionTitle: 'Latest Work',
-            cardTitle: 'Simple Blog',
-            text: '',
-            follow: 'See Portfolio',
+            contentTitle: 'Simple Blog',
+            contentText: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen ',
+            link: 'See Portfolio',
             other: ''
         }, {
             sectionTitle: 'Latest Blog Post',
-            cardTitle: 'Learn to use helpers in Ember',
-            text: '',
-            follow: 'Go to Blog',
+            contentTitle: 'Learn to use helpers in Ember',
+            contentText: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen',
+            link: 'Go to Blog',
             other: ''
         }, {
             sectionTitle: 'About Me',
-            cardTitle: 'Front End Everything',
-            text: '',
-            follow: 'Get to Know Me',
+            contentTitle: 'Front End Everything',
+            contentText: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen ',
+            link: 'Get to Know Me',
             other: ''
         }];
 
