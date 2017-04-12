006bf2952f9837c384a64840ab04219dbc410b02
felipe <felips.110@gmail.com>
2017-04-11 23:30:39 -0700

    progress

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,22 +3,24 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model: function() {
         var introCardsContent = [{
-            sectionTitle: 'Recent Creation',
+            sectionTitle: 'Latest Project',
             contentTitle: 'Simple Blog',
-            contentText: 'An API based blog with CRUD operations and JWT authentication based on Ember.',
-            link: 'See Portfolio',
+            contentText: 'An API based blog application with CRUD operations and JWT authentication based on Ember.',
+            link: 'See All Projects',
             other: '',
             theme: 'yellow',
             img: '/images/blog-screen.png',
-            state: 'blog'
+            state: 'login',
+            params: false
         }, {
-            sectionTitle: 'Latest Blog',
+            sectionTitle: 'Last Blog',
             contentTitle: 'Learn a Framework By Writting A Blog',
             contentText: 'Buidling a blog app to get the basics and insights of any framework',
             link: 'Go to Blog',
             theme: 'green',
             img: 'http://i.imgur.com/FaP3aA1.png',
-            state: 'blog'
+            state: 'blog.post',
+            params: 75
         }, {
             sectionTitle: 'About Me',
             contentTitle: 'Hi I\'m Phil,',
@@ -27,7 +29,8 @@ export default Ember.Route.extend({
             link: 'Learn More',
             theme: 'blue',
             img: 'http://i.imgur.com/GKPMjkr.jpg',
-            state: 'blog'
+            state: 'blog',
+            params: false
         }];
 
         return introCardsContent
