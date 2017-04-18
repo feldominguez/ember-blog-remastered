0de1420413ea3626259a49fbfb23abee3bf8aff4
felipe <felips.110@gmail.com>
2017-04-17 21:51:27 -0700

    portfolio progess

--- app/routes/portfolio/index.js
+++ app/routes/portfolio/index.js
@@ -4,55 +4,35 @@ export default Ember.Route.extend({
     model: function() {
         const projectCards = [{
             name: 'simple-blog',
-            link: 'simple-blog',
             contentTitle: 'Simple Blog',
             shortText: 'API based blog application with CRUD operations and JWT authentication based on Ember.',
             detailText: '',
             currentLink: 'See Project',
-            linsk: 'See All Projects',
-            other: '',
-            theme: 'yellow',
             img: '/images/blog-screen.png',
-            state: 'simple-blog',
             params: false
         }, {
             name: 'simple-blog',
-            link: 'simple-blog',
             contentTitle: 'Simple Blog',
             shortText: 'API based blog application with CRUD operations and JWT authentication based on Ember.',
             detailText: '',
             currentLink: 'See Project',
-            linsk: 'See All Projects',
-            other: '',
-            theme: 'yellow',
             img: '/images/blog-screen.png',
-            state: 'simple-blog',
             params: false
         }, {
             name: 'simple-blog',
-            link: 'simple-blog',
             contentTitle: 'Simple Blog',
             shortText: 'API based blog application with CRUD operations and JWT authentication based on Ember.',
             detailText: '',
             currentLink: 'See Project',
-            linsk: 'See All Projects',
-            other: '',
-            theme: 'yellow',
             img: '/images/blog-screen.png',
-            state: 'simple-blog',
             params: false
         }, {
             name: 'simple-blog',
-            link: 'simple-blog',
             contentTitle: 'Simple Blog',
             shortText: 'API based blog application with CRUD operations and JWT authentication based on Ember.',
             detailText: '',
             currentLink: 'See Project',
-            linsk: 'See All Projects',
-            other: '',
-            theme: 'yellow',
             img: '/images/blog-screen.png',
-            state: 'simple-blog',
             params: false
         }, ];
 
