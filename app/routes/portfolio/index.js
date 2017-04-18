90cb79929a34fef8433ec3ff3edd47c2daf86b8f
felipe <felips.110@gmail.com>
2017-04-17 22:35:03 -0700

    fixing object content

--- app/routes/portfolio/index.js
+++ app/routes/portfolio/index.js
@@ -11,25 +11,25 @@ export default Ember.Route.extend({
             img: '/images/blog-screen.png',
             params: false
         }, {
-            name: 'simple-blog',
-            contentTitle: 'Simple Blog',
-            shortText: 'API based blog application with CRUD operations and JWT authentication based on Ember.',
+            name: 'weather-widget',
+            contentTitle: 'Weather Widget',
+            shortText: 'Responsive Weather Widget created with the Aeris API and Meterial-y designed.',
             detailText: '',
             currentLink: 'See Project',
             img: '/images/blog-screen.png',
             params: false
         }, {
-            name: 'simple-blog',
-            contentTitle: 'Simple Blog',
-            shortText: 'API based blog application with CRUD operations and JWT authentication based on Ember.',
+            name: 'reddit-search',
+            contentTitle: 'Reddit Search',
+            shortText: 'Responsive sub-reddit search created with Handlebars.js, Reddit API, and Bootstrap.',
             detailText: '',
             currentLink: 'See Project',
             img: '/images/blog-screen.png',
             params: false
         }, {
-            name: 'simple-blog',
-            contentTitle: 'Simple Blog',
-            shortText: 'API based blog application with CRUD operations and JWT authentication based on Ember.',
+            name: 'twitch-staus-check',
+            contentTitle: 'Twitch Status Checker',
+            shortText: 'Responsive Twitch TV App built with the Twitch API.',
             detailText: '',
             currentLink: 'See Project',
             img: '/images/blog-screen.png',
--- app/routes/portfolio/project.js
+++ app/routes/portfolio/project.js
@@ -9,39 +9,31 @@ export default Ember.Route.extend({
             detailText: '',
             currentLink: 'See Project',
             img: '/images/blog-screen.png',
-            state: 'simple-blog',
-            params: false,
-            project: 'simple-blog',
+            params: false
         }, {
-            name: 'simple-blog',
-            contentTitle: 'Simple Blog',
-            shortText: 'API based blog application with CRUD operations and JWT authentication based on Ember.',
+            name: 'weather-widget',
+            contentTitle: 'Weather Widget',
+            shortText: 'Responsive Weather Widget created with the Aeris API and Meterial-y designed.',
             detailText: '',
             currentLink: 'See Project',
             img: '/images/blog-screen.png',
-            state: 'simple-blog',
-            params: false,
-            project: 'simple-blog',
+            params: false
         }, {
-            name: 'simple-blog',
-            contentTitle: 'Simple Blog',
-            shortText: 'API based blog application with CRUD operations and JWT authentication based on Ember.',
+            name: 'reddit-search',
+            contentTitle: 'Reddit Search',
+            shortText: 'Responsive sub-reddit search created with Handlebars.js, Reddit API, and Bootstrap.',
             detailText: '',
             currentLink: 'See Project',
             img: '/images/blog-screen.png',
-            state: 'simple-blog',
-            params: false,
-            project: 'simple-blog',
+            params: false
         }, {
-            name: 'simple-blog',
-            contentTitle: 'Simple Blog',
-            shortText: 'API based blog application with CRUD operations and JWT authentication based on Ember.',
+            name: 'twitch-staus-check',
+            contentTitle: 'Twitch Status Checker',
+            shortText: 'Responsive Twitch TV App built with the Twitch API.',
             detailText: '',
             currentLink: 'See Project',
             img: '/images/blog-screen.png',
-            state: 'simple-blog',
-            params: false,
-            project: 'simple-blog',
+            params: false
         }, ];
 
 
