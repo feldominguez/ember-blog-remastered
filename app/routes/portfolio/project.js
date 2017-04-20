fef65251995a30761297d188d3d7dfb92e2566b3
felipe <felips.110@gmail.com>
2017-04-19 19:47:41 -0700

    styling project page

--- app/routes/portfolio/project.js
+++ app/routes/portfolio/project.js
@@ -6,33 +6,34 @@ export default Ember.Route.extend({
             name: 'simple-blog',
             contentTitle: 'Simple Blog',
             shortText: 'API based blog application with CRUD operations and JWT authentication based on Ember.',
-            detailText: '',
+            liveProject: 'https://google.com',
             currentLink: 'See Project',
-            img: '/images/blog-screen.png',
-            params: false
+            img: 'https://i.imgur.com/zVbhBFs.png',
+            params: false,
+            projectTech: ['CSS','JS','jQuery','HTML','EMBER']
         }, {
             name: 'weather-widget',
-            contentTitle: 'Weather Widget',
+            contentTitle: 'Weather<br>Widget',
             shortText: 'Responsive Weather Widget created with the Aeris API and Meterial-y designed.',
-            detailText: '',
+            liveProject: 'https://google.com',
             currentLink: 'See Project',
-            img: '/images/blog-screen.png',
+            img: 'https://i.imgur.com/oOC2Fds.png',
             params: false
         }, {
             name: 'reddit-search',
             contentTitle: 'Reddit Search',
             shortText: 'Responsive sub-reddit search created with Handlebars.js, Reddit API, and Bootstrap.',
-            detailText: '',
+            liveProject: 'https://google.com',
             currentLink: 'See Project',
-            img: '/images/blog-screen.png',
+            img: 'https://i.imgur.com/WmFoZJX.png',
             params: false
         }, {
             name: 'twitch-staus-check',
             contentTitle: 'Twitch Status Checker',
             shortText: 'Responsive Twitch TV App built with the Twitch API.',
-            detailText: '',
+            liveProject: 'https://google.com',
             currentLink: 'See Project',
-            img: '/images/blog-screen.png',
+            img: 'https://i.imgur.com/bjFOmVj.png',
             params: false
         }, ];
 
